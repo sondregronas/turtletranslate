e77090da51c0a339287fac6e98c7393d1b5f2712
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2025-03-20 15:55:01 +0100

    chore: modify frontmatter prompt slightly

--- turtletranslate/models/frontmatter.py
+++ turtletranslate/models/frontmatter.py
@@ -1,5 +1,5 @@
 FRONTMATTER_WORKER_SYSTEM = """
-You are an expert markdown translator specializing in JSON content, specifically translating the **values** of JSON objects while leaving the **keys** unchanged. Ensure that only the values are translated from {source_language} to {target_language}, and preserve the overall JSON structure. Ensure values always end with a double quote (") or double quote followed by a comma (",)."""
+You are an expert markdown translator specializing in JSON content, specifically translating the **values** of JSON objects while leaving the **keys** unchanged. Ensure that only the values are translated from {source_language} to {target_language}, and preserve the overall JSON structure. Ensure values always end with a quote and a comma."""
 
 FRONTMATTER_WORKER_PROMPT = """\
 Translate the JSON values from {source_language} to {target_language}. Respond with the translated JSON object, ensuring that only the values are modified, while the keys and overall structure remain intact.
@@ -9,7 +9,7 @@ Translate the JSON values from {source_language} to {target_language}. Respond w
 2. Ensure that the translation of the values is accurate and natural in {target_language}.
 3. Preserve the overall structure of the JSON, including nested objects and arrays.
 4. Ensure that no additional formatting or changes to the JSON structure are made.
-5. Ensure values always end with a double quote (") or double quote followed by a comma (",).
+5. Ensure values always end with a quote and a comma.
 
 **Original JSON:**
 {frontmatter}
