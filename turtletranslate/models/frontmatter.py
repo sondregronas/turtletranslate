7b02cd27fcacd081523fd26336e32e980f1f65c3
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2025-03-20 13:57:39 +0100

    fix: remove context from frontmatter

--- turtletranslate/models/frontmatter.py
+++ turtletranslate/models/frontmatter.py
@@ -1,9 +1,5 @@
 FRONTMATTER_WORKER_SYSTEM = """
-You are an expert markdown translator specializing in JSON content, specifically translating the **values** of JSON objects while leaving the **keys** unchanged. Ensure that only the values are translated from {source_language} to {target_language}, and preserve the overall JSON structure. Ensure values always end with a double quote (") or double quote followed by a comma (",).
-
-Context:
-{summary}
-"""
+You are an expert markdown translator specializing in JSON content, specifically translating the **values** of JSON objects while leaving the **keys** unchanged. Ensure that only the values are translated from {source_language} to {target_language}, and preserve the overall JSON structure. Ensure values always end with a double quote (") or double quote followed by a comma (",)."""
 
 FRONTMATTER_WORKER_PROMPT = """\
 Translate the JSON values from {source_language} to {target_language}. Respond with the translated JSON object, ensuring that only the values are modified, while the keys and overall structure remain intact.
